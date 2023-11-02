17f4a49767f4971f0ecf2fbea337d200bae84939
Abdul <140862207+abdul2811@users.noreply.github.com>
2023-11-02 11:06:29 -0400

    ui added

--- main.js
+++ main.js
@@ -1,3 +1,5 @@
+const container = document.querySelector('div');
+
 const body = document.querySelector('body');
 
 const rock = document.createElement('button');
@@ -9,59 +11,99 @@ paper.textContent = "Paper";
 const scissors = document.createElement('button');
 scissors.textContent = "Scissors";
 
+// const container = document.querySelector('div');
+
+
 body.appendChild(rock);
 body.appendChild(paper);
 body.appendChild(scissors);
+// body.appendChild(container);
+
 
 
 const RPS = ["rock", "paper", "scissors"];
-// let compScore = 0;
-// let playerScore = 0;
+let compScore = 0;
+let playerScore = 0;
 
 function getComputerChoice() {
     return RPS[(Math.floor(Math.random() * RPS.length))]
 }
 
-function getPlayerChoice() {
-    let choice = prompt("rock, paper, or scissors?");
-    while (choice.toLowerCase() != "rock" && choice.toLowerCase() != "paper" && choice.toLowerCase() != "scissors") {
-        choice = prompt("Enter a valid choice:");
+rock.addEventListener('click', () => {
+    if (compScore != 5 && playerScore != 5){
+        game("rock")
     }
-    return choice.toLowerCase()
-}
+})
 
-function game() {
-    let compScore = 0;
-    let playerScore = 0;
+paper.addEventListener('click', () => {
+    if (compScore != 5 && playerScore != 5){
+        game("paper")
+    }
+})
 
-    while (compScore != 5 && playerScore != 5) {
+scissors.addEventListener('click', () => {
+    if (compScore != 5 && playerScore != 5){
+        game("scissors")
+    }
+})
+
+
+
+// function getPlayerChoice() {
+//     let choice = prompt("rock, paper, or scissors?");
+//     while (choice.toLowerCase() != "rock" && choice.toLowerCase() != "paper" && choice.toLowerCase() != "scissors") {
+//         choice = prompt("Enter a valid choice:");
+//     }
+//     return choice.toLowerCase()
+// }
+
+function game(player) {
+
+    // while (compScore != 5 && playerScore != 5) {
+
         let comp = getComputerChoice();
-        let player = getPlayerChoice();
 
         if (player === "rock") {
             if (comp === "rock") {
-                console.log("It's a tie!");
+                container.textContent = `It's a tie! ${playerScore}-${compScore}`;
             } else if (comp === "scissors") {
                 playerScore += 1;
-                console.log("Rock beats scissors, you win!");
+                container.textContent =  `Rock beats scissors, you win! ${playerScore}-${compScore}`;
             } else if (comp === "paper") {
                 compScore += 1;
-                console.log("Paper beats rock, you lose!");
+                container.textContent = `Paper beats rock, you lose! ${playerScore}-${compScore}`;
             }
         } else if (player === "paper") {
-            // Handle the other cases similarly
+            if (comp === "rock") {
+                playerScore += 1
+                container.textContent = `Paper beats rock, you win! ${playerScore}-${compScore}`;
+            } else if (comp === "scissors") {
+                compScore += 1;
+                container.textContent =  `Scissors beats paper, you lose! ${playerScore}-${compScore}`;
+            } else if (comp === "paper") {
+                container.textContent = `It's a tie! ${playerScore}-${compScore}`;
+            }
         } else if (player === "scissors") {
-            // Handle the other cases similarly
+            if (comp === "rock") {
+                compScore += 1
+                container.textContent = `Rock beats scissors, you lose! ${playerScore}-${compScore}`;
+            } else if (comp === "scissors") {
+                container.textContent =  `It's a tie! ${playerScore}-${compScore}`;
+            } else if (comp === "paper") {
+                playerScore += 1
+                container.textContent = `Scissors beats paper, you win! ${playerScore}-${compScore}`;
+            }
         }
 
-        console.log(`${playerScore}-${compScore}`);
+        // console.log(`${playerScore}-${compScore}`);
     }
 
     if (playerScore === 5) {
-        return "You Win!";
-    } else {
-        return "You Lose!";
+        container.textContent = "You Win!";
+    } else if (compScore === 5) {
+        container.textContent = "You Lose!";
     }
-}
+
+// }
 
 // console.log(game());
